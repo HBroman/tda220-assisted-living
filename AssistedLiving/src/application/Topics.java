9a47e8cf9b42da86e70a91d5ecd61c4993ed4c9e
Hampus Broman <namorben@gmail.com>
2020-03-28 17:26:56 +0100

    minor

--- AssistedLiving/src/application/Topics.java
+++ AssistedLiving/src/application/Topics.java
@@ -5,7 +5,7 @@ public class Topics {
     public static final String MEDICAL_DEVICE = "medicaldevice/data"; //data from the medical device, goes to bluetooth hub (int numbersteps/ int currentpulse)
     public static final String HEALTH_INFO = "health/info"; //data from bluetooth hub to dashboard (int totalstepstoday/ double averagepulsetoday)
     public static final String LOCK = "lock";
-    public static final String BROKER_URL = "tcp://localhost:1883";
+    public static final String BROKER_URL = "tcp://mqtt.eclipse.org:1883"; //"tcp://localhost:1883";
     public static final String MOVEMENT_SECURITY = "security"; // (movement/name/boolean)
     public static final String MOVEMENT_SAFETY = "movement/safety";
     public static final String SMOKE_SAFETY = "smoke/safety";
